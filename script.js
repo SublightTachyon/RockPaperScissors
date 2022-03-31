2152658f35a39814b7f9f4f9d75a3375ff4621bd
SublightTachyon <Turone2021@gmail.com>
2022-03-31 00:37:01 -0500

    Mar 31 12:30 am

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 let playerScore = 0
 let computerScore = 0
-    let rock = 'ROCK';
-    let paper = 'PAPER';
-    let scissors = 'SCISSORS';
-    /*let randNum= Math.random();
-     ComputerPlay(){
-    if (randNum > 0.33) {
-        return  rock;
-    }   else if (randNum  > 0.66){
-        return  paper;
-    }   else {
-        return  scissors;
-    }
-
-    
-}*/
+let rock = 'ROCK';
+let paper = 'PAPER';
+let scissors = 'SCISSORS';
 const crps = ['ROCK','PAPER','SCISSORS'];
 function computerPlay() {
     return crps[Math.floor(Math.random() * crps.length)]
 }
+const playerSelection = prompt('Rock, Paper, or Scissors','Rock'.toUpperCase()) ;
+const computerSelection = computerPlay()
 //console.log(computerPlay())
 function playRound(playerSelction, computerSelection){
     let draw = "It's a draw"
@@ -58,21 +48,17 @@ function playRound(playerSelction, computerSelection){
         return 'ERROR CODER ONE'
     }
 }
-const playerSelection = prompt('Rock, Paper, or Scissors','Rock'.toUpperCase()) ;
-const computerSelection = computerPlay()
-//console.log(playRound(playerSelection, computerSelection))
-//let playerScore = 0
-//let computerScore = 0
+
+console.log(playRound(playerSelection, computerSelection))
 function game(){
-    return playRound(playerSelection, computerSelection)/*
-} if (playerSelection == 'Rock' && computerSelection == 'Scissors' || playerSelection == 'Scissors' && computerSelection == 'Paper'|| playerSelection == 'Paper' && computerSelection == 'Rock'){
-    playerScore += 1
-} else if (playerSelection == 'Rock' && computerSelection == 'Paper' || playerSelection == 'Scissors' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Scissors'){
-    computerScore += 1
-} else tie += 1 */
-while (computerScore < 3 || playerScore < 3){
-    playRound()
+        do {
+        if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS' || playerSelection == 'PAPER' && computerSelection == 'ROCK' || playerSelection == 'SCISSORS' && computerSelection == 'PAPER'){
+            playerScore ++
+        } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER'||playerSelection == 'PAPER' && computerSelection == 'SCISSORS'||playerSelection == 'SCISSORS' && computerSelection == 'ROCK'){
+            computerScore++
+        } else tie++
+    } 
+    while (computerScore < 3|| playerScore < 3)
+    return playRound(playerSelction, computerSelection)
 }
 console.log(game())
-/* write a function called game() the keeps track of the score an plays best out of five round of Rock, Paper, Scissors.
-call playround inside game. check status of playRound*/
