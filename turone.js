6a505d50c7084ba3dc81cef15f43b87e6c577c8b
SublightTachyon <Turone2021@gmail.com>
2022-04-05 21:51:25 -0500

    progress in turone.js

--- turone.js
+++ turone.js
@@ -1,10 +1,17 @@
 //declare some variables that will be used in the code.
 const shoot = ['rock','paper','scissors']
-const Cselect = comPlay()
+var Cselect = comPlay()
+var Pselect = humanPlay()
+var draw = 0
+var loss = 0
+var win = 0
 // function comPlay that returns rock, paper or scissors
+
 function comPlay(){
     return shoot[Math.floor(Math.random()* shoot.length)]
 }
+
+//Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 function humanPlay(){
     let choice = window.prompt('rock, paper, or scissors','rock').toLowerCase();
     while (
@@ -12,10 +19,35 @@ function humanPlay(){
     ){
         choice = window.prompt('you have to type rock, paper, or scissors. Correct spelling, case insenstive.')
     } return choice;
-   
 }
-console.log(humanPlay())
-// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+console.log('Player chose:',Pselect)
+console.log('Computer chose:',Cselect)
+// Write a function that plays a single round of Rock Paper Scissors. 
+//The function should take two parameters - the playerSelection and computerSelection - 
+//and then return a string that declares the winner of the round like so:
+// "You Lose! Paper beats Rock"
+function singleRound(Pselect,Cselect){
+    if (Pselect = Cselect){
+        console.log('Tie');
+        return draw
+    } else if (
+        (Pselect === 'rock' && Cselect === 'scissors')||
+        (Pselect === 'paper' && Cselect === 'rock')||
+        (Pselect === 'scissors' && Cselect === 'paper')
+    ) {
+        console.log('You Win')
+        return win
+    } else if (
+        (Pselect === 'rock' && Cselect === 'paper') ||
+        (Pselect === 'paper' && Cselect ==='scissors') ||
+        (Pselect === 'scissors' && Cselect === 'rock')
+    ) {
+        console.log('You lose');
+        return loss
+    } else {console.log('ERROR FUNCTION SINGLE ROUND')}
+}
+console.log(singleRound(Pselect,Cselect))
+
 //Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
 
 //function playRound(comPlay,humanPlay)
